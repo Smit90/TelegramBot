7d1fa48eb52750ae6e1f278d339f97ff1abdbf90
Smit Patel <smitp90333@gmail.com>
2021-02-16 18:43:12 +0530

    add new commands

--- index.js
+++ index.js
@@ -55,10 +55,65 @@ bot.onText(/\/weather (.+)/, function (msg, match) {
                     const wind_kph = parseFloat(res.current.wind_kph)
                     const cloud = parseInt(res.current.cloud)
                     const icon = res.current.condition.icon.replace('//', '')
-                    bot.sendPhoto(fromId, icon, {caption: `${city_name} | ${state_name} | ${country_name} \nLocal Time: ${local_time} \nTemp: ${temp_c}°C | ${temp_f}°F \nCondition: ${condition_text} | Humidity: ${humidity} \nWind Speed: ${wind_kph} | Cloud: ${cloud}` })
+                    bot.sendPhoto(fromId, icon, { caption: `${city_name} | ${state_name} | ${country_name} \nLocal Time: ${local_time} \nTemp: ${temp_c}°C | ${temp_f}°F \nCondition: ${condition_text} | Humidity: ${humidity} \nWind Speed: ${wind_kph} | Cloud: ${cloud}` })
                 })
         }
     })
+});
+
+
+bot.on('message', (msg) => {
+    var Hi = "hi";
+    if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
+        bot.sendMessage(msg.chat.id, "Hello dear " + msg.from.first_name + ' ' + msg.from.last_name);
+    }
+    var bye = "bye";
+    if (msg.text.toString().toLowerCase().includes(bye)) {
+        bot.sendMessage(msg.chat.id, "Hope to see you around again , Bye");
+    }
+    var robot = "are you a robot?";
+    if (msg.text.toString().toLocaleLowerCase().includes(robot)) {
+        bot.sendMessage(msg.chat.id, "Yes I'm robot but not in that way!");
+    }
+    var location = "location";
+    if (msg.text.toLocaleLowerCase().indexOf(location) === 0) {
+        bot.sendLocation(msg.chat.id, 21.1777104, 72.8430127);
+        bot.sendMessage(msg.chat.id, "Here is the point");
+
+    }
+    var Help = 'help'
+    if (msg.text.toLocaleLowerCase().indexOf(Help) === 0) {
+        bot.sendMessage(msg.chat.id, "Welcome", {
+            "reply_markup": {
+                "keyboard": [["hi", "computer"], ["Location"], ["Bye"]]
+            }
+        });
+    }
+    var Gtu = 'send me gtu syllabus'
+    if (msg.text.toLocaleLowerCase().indexOf(Gtu) === 0) {
+        bot.sendMessage(msg.chat.id, "Here it is, Select your department", {
+            "reply_markup": {
+                "keyboard": [["computer", "Chemical", "Robotics"], ["Civil", "Electircal", "IT"]]
+            }
+        });
+    }
+    var computer = 'computer'
+    if (msg.text.toLocaleLowerCase().indexOf(computer) === 0) {
+        bot.sendMessage(msg.chat.id, "choose your sem", {
+            "reply_markup": {
+                "keyboard": [["1", "2", "3"],["4", "5", "6"],["7", "8", "9"]]
+            }
+        });
+    }
+    var eight = '8'
+    if (msg.text.toLocaleLowerCase().indexOf(eight) === 0) {
+        bot.sendMessage(msg.chat.id, "List of subjects", {
+            "reply_markup": {
+                "keyboard": [["AI"],["Python Programming"],["Project (Pahse II)"]]
+            }
+        });
+    }
+
 });
 
 
